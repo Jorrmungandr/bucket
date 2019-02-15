96d388bf62ae4f98181eee57bdb9da340be8807d
Yorrmungand <edgarmarques@hotmail.com.br>
2019-02-15 09:22:28 -0300

    added stopFollowingMouse func

--- static/app.js
+++ static/app.js
@@ -2,18 +2,18 @@ let ball = document.getElementById('ball');
 let coords;
 let flag = false;
 
-const showCoords = () => {
-    coords = 'X:' + event.clientX + ', Y:' + event.clientY
-    document.getElementById('coordsContainer').innerHTML = coords;
+const moveDiv = () => {
+        coords = 'X:' + event.clientX + ', Y:' + event.clientY;
+        document.getElementById('coordsContainer').innerHTML = coords;
+        ball.style.left = (event.clientX - 150 + 'px');
+        ball.style.top = (event.clientY - 150 + 'px');
 }
+
 //on mouse down
 const followMouse = () => {
-    document.addEventListener("mousemove", () => {
-        coords = 'X:' + event.clientX + ', Y:' + event.clientY
-        document.getElementById('coordsContainer').innerHTML = coords;
-    })
+    document.addEventListener("mousemove", moveDiv)
 };
 //on mouse up
-const mouseUp = () => {
-    document.removeEventListener("mousemove", showCoords())
+const stopFollowingMouse = () => {
+    document.removeEventListener("mousemove", moveDiv)
 }
