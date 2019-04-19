cc3c09fde0dfdf25da132fbf4ca422309166d87b
Yorrmungand <edgarmarques@hotmail.com.br>
2019-04-19 09:24:27 -0300

    Added Comments

--- static/app.js
+++ static/app.js
@@ -1,57 +1,58 @@
-let ball = document.getElementById('ball');
+let ball = document.getElementById('ball').style;
 let water = document.getElementById('water');
-let coords;
-let flag = 1;
 let fallSpeed = 0;
-let topDis = 0;
+let coords;
+// the middle of the screen
 let mainY = '167px';
 let mainX = '512.5px';
+let flag = true;
 let gravityOn = false;
 
 const gravity = 0.5;
-
+// Move Drop of water
 const moveDiv = () => {
     if (gravityOn === false) {
         coords = 'X:' + event.clientX + ', Y:' + event.clientY;
         document.getElementById('coordsContainer').innerHTML = coords;
-        ball.style.left = (event.clientX - 5 + 'px');
-        ball.style.top = (event.clientY - 5 + 'px');
-    }   
+        ball.left = (event.clientX - 5 + 'px');
+        ball.top = (event.clientY - 5 + 'px');
+    }
 }
-
+// putting the drop back in its place
+const reset = () => {
+    fallSpeed = 0;
+    flag = false;
+    ball.top = mainY;
+    ball.left = mainX;
+}
+//The drop starts to fall
 const gravityApply = () => {
     fallSpeed += gravity;
-    topDis = parseFloat(ball.style.top, 10);
-    leftDis = parseFloat(ball.style.left, 10);
-    ball.style.top = topDis + fallSpeed + 'px';
-    if (topDis >= 500 - water.clientHeight & leftDis > 669 & leftDis < 758) {
+    let topDis = parseFloat(ball.top, 10);
+    let leftDis = parseFloat(ball.left, 10);
+    ball.top = topDis + fallSpeed + 'px'
+    if (topDis >= 500 - water.clientHeight & leftDis > 668 & leftDis < 758) {
         if (water.clientHeight <= 185) {
             water.style.height = water.clientHeight + 5 + 'px';
-            fallSpeed = 0;
-            flag = 0;
-            ball.style.top = mainY;
-            ball.style.left = mainX;
+            reset();
         }
     }
     if (topDis >= 585.93) {
-        fallSpeed = 0;
-        flag = 0;
-        ball.style.top = mainY;
-        ball.style.left = mainX;
+        reset();
     }
 }
 //on mouse down
 const followMouse = () => {
     document.addEventListener("mousemove", moveDiv);
 
-    flag = 1;
+    flag = true;
 };
 //on mouse up
 const stopFollowingMouse = () => {
     document.removeEventListener("mousemove", moveDiv);
     // gravity
     setInterval(() => {
-        if (flag === 1) {
+        if (flag) {
             gravityApply();
             gravityOn = true;
         }
